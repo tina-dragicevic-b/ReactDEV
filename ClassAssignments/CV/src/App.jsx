e0e7e2e59a264d04cf8219a99a7185778230dbac
tina-dragicevic <tina.dragicevic3108@gmail.com>
2023-03-14 14:08:30 +0100

    Fixed border and margin

--- ClassAssignments/CV/src/App.jsx
+++ ClassAssignments/CV/src/App.jsx
@@ -10,36 +10,43 @@ function App() {
   const projects = [
     {
       name: "Project One",
-      projectImage: "https://img.graphicsurf.com/2018/11/project-management-vector-free-icons-set2.jpg",
+      projectImage:
+        "https://img.graphicsurf.com/2018/11/project-management-vector-free-icons-set2.jpg",
       description: "Guia voluptas sit.",
-      author: "Tina"
-    },{
+      author: "Tina",
+    },
+    {
       name: "Project Two",
-      projectImage: "https://blog.topdesk.com/nl/wp-content/uploads/sites/14/2022/03/AM-2022_Visual-Blog-01-e1664529576929.png",
+      projectImage:
+        "https://blog.topdesk.com/nl/wp-content/uploads/sites/14/2022/03/AM-2022_Visual-Blog-01-e1664529576929.png",
       description: "Ratione voluptatem sequi.",
-      author: "Tina"
-    },{
+      author: "Tina",
+    },
+    {
       name: "Project Three",
-      projectImage: "https://img.freepik.com/premium-vector/illustration-downloading-graphic-design-internet-server-network-down-arrow-download_4968-1026.jpg",
+      projectImage:
+        "https://img.freepik.com/premium-vector/illustration-downloading-graphic-design-internet-server-network-down-arrow-download_4968-1026.jpg",
       description: "Neque porro quisquam est.",
-      author: "Tina"
+      author: "Tina",
     },
     // {
     //   name: "P3",
     //   projectImage: "https://img.freepik.com/premium-vector/illustration-downloading-graphic-design-internet-server-network-down-arrow-download_4968-1026.jpg",
-    //   description: ""
+    //   description: "",
+    // author: "Tina"
     // },{
     //   name: "P3",
     //   projectImage: "https://img.freepik.com/premium-vector/illustration-downloading-graphic-design-internet-server-network-down-arrow-download_4968-1026.jpg",
-    //   description: ""
+    //   description: "",
+    // author: "Tina"
     // },
-  ]
+  ];
 
   return (
     <>
       <Header></Header>
       <AboutUser></AboutUser>
-      <div className="grid grid-cols-3 xsm:grid-cols-1 justify-items-center mt-32 mb-36">
+      <div className="grid grid-cols-3 xsm:grid-cols-1 justify-items-center mt-16 mb-36">
       {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-6 p-2 md:p-6"> */}
         {projects.map((project) => (
           <Project key={project.name} project={project}></Project>
--- ClassAssignments/CV/src/components/Project.jsx
+++ ClassAssignments/CV/src/components/Project.jsx
@@ -7,7 +7,7 @@ function Project({project}) {
         <div className="project bg-white w-96 m-5 rounded-lg">
           <div>
           {/* <div className="w-full h-60 lg:w-80 sm:w-60"> */}
-            <img className="object-contain" src={project.projectImage} />
+            <img className="object-contain rounded-t-lg " src={project.projectImage} />
           </div>
           <h1 className="text-gray-900 m-1 font-mono font-extrabold">{project.name}</h1>
           <h3 className="text-gray-600 ml-1 m-3 font-mono">{project.description} by {project.author}</h3>
